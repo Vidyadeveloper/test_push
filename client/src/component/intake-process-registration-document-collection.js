e1dc2d33bce5bbe436327c6bbb6486ccb4cbda6d
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-07-23 17:31:19 +0200

    pushued folder

--- client/src/component/intake-process-registration-document-collection.js
+++
@@ -1,8 +0,0 @@
-
-  import React from 'react';
-
-  const intake-process-registration-document-collection = () => {
-    return <div>intake-process-registration-document-collection view placeholder</div>;
-  };
-
-  export default intake-process-registration-document-collection;
