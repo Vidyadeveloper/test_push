ac51324de4704c3be0167fe745ebfdaa77d75511
Vidyadeveloper <Vidyadeveloper@blaze-sync.local>
2025-07-24 13:15:45 +0000

    🔄 Synced components & case types [2025-07-24T13:15:45.036Z]

---
+++ core/case-type/verification.js
@@ -0,0 +1,22 @@
+// Case Type: verification
+module.exports = {
+  "caseType": {
+    "id": "verification",
+    "label": "Verification"
+  },
+  "stages": [
+    {
+      "id": "stage1",
+      "label": "Stage 1",
+      "steps": [
+        {
+          "id": "step1",
+          "label": "Step 1",
+          "status": "pending",
+          "view": "summary",
+          "type": "manual"
+        }
+      ]
+    }
+  ]
+};
