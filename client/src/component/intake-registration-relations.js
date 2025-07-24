e9fa41031f41a5e8192dcbec8964e5bf16c1987f
Vidyadeveloper <Vidyadeveloper@blaze-sync.local>
2025-07-24 08:49:14 +0000

    🆕 Synced views & case types for test_push

--- client/src/component/intake-registration-relations.js
+++ client/src/component/intake-registration-relations.js
@@ -1,8 +1,1 @@
-
-  import React from 'react';
-
-  const intake-registration-relations = () => {
-    return <div>intake-registration-relations view placeholder</div>;
-  };
-
-  export default intake-registration-relations;
+// No content
