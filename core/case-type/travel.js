44733f1120bab332344d7ac49d2f06abde78e61e
Vidyadeveloper <Vidyadeveloper@blaze-sync.local>
2025-07-30 15:09:17 +0000

    🔄 Synced components & case types [2025-07-30T15:09:16.310Z]

---
+++ core/case-type/travel.js
@@ -0,0 +1,22 @@
+// Case Type: travel
+module.exports = {
+  "caseType": {
+    "id": "travel",
+    "label": "Travel"
+  },
+  "stages": [
+    {
+      "id": "stage1",
+      "label": "Stage 1",
+      "steps": [
+        {
+          "id": "step1",
+          "label": "Step 1",
+          "status": "pending",
+          "view": "summary",
+          "type": "manual"
+        }
+      ]
+    }
+  ]
+};
